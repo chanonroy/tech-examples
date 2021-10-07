0f06c438edc833f551d7b43fcee83790e4072308
Chanon Roy <chanon.roy@shopify.com>
2021-10-07 10:47:02 +0100

    feat: add cart slice fulfilled action

--- redux-toolkit-example/src/features/cart/cartSlice.ts
+++ redux-toolkit-example/src/features/cart/cartSlice.ts
@@ -1,26 +1,34 @@
-import { createAsyncThunk, createSelector, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {
+  createAsyncThunk,
+  createSelector,
+  createSlice,
+  PayloadAction,
+} from "@reduxjs/toolkit";
 import { CartItems, checkout } from "../../app/api";
 import { RootState } from "../../app/store";
 
-type CheckoutState = "LOADING" | "READY" | "ERROR"
+type CheckoutState = "LOADING" | "READY" | "ERROR";
 
 export interface CartState {
   items: { [productID: string]: number };
-  checkoutState: CheckoutState
+  checkoutState: CheckoutState;
   errorMessage: string;
 }
 
 const initialState: CartState = {
   items: {},
   checkoutState: "READY",
-  errorMessage: ""
+  errorMessage: "",
 };
 
 // This is really cool
-export const checkoutCart = createAsyncThunk("cart/checkout", async (items: CartItems) => {
-  const response = await checkout(items);
-  return response
-})
+export const checkoutCart = createAsyncThunk(
+  "cart/checkout",
+  async (items: CartItems) => {
+    const response = await checkout(items);
+    return response;
+  }
+);
 
 const cartSlice = createSlice({
   name: "cart",
@@ -35,25 +43,37 @@ const cartSlice = createSlice({
       }
     },
     removeFromCart(state, action: PayloadAction<string>) {
-      delete state.items[action.payload]
+      delete state.items[action.payload];
     },
-    udpateQuantity(state, action: PayloadAction<{ id: string, quantity: number }>) {
+    udpateQuantity(
+      state,
+      action: PayloadAction<{ id: string; quantity: number }>
+    ) {
       const { id, quantity } = action.payload;
-      state.items[id] = quantity
-    }
+      state.items[id] = quantity;
+    },
   },
-  extraReducers: function(builder) {
+  extraReducers: function (builder) {
     builder.addCase(checkoutCart.pending, (state) => {
-      state.checkoutState = "LOADING"
-    })
-    builder.addCase(checkoutCart.fulfilled, (state) => {
-      state.checkoutState = "READY"
-    })
+      state.checkoutState = "LOADING";
+    });
+    builder.addCase(
+      checkoutCart.fulfilled,
+      (state, action: PayloadAction<{ success: boolean }>) => {
+        const { success } = action.payload;
+        if (success) {
+          state.checkoutState = "READY";
+          state.items = {};
+        } else {
+          state.checkoutState = "ERROR"
+        }
+      }
+    );
     builder.addCase(checkoutCart.rejected, (state, action) => {
-      state.checkoutState = "ERROR"
-      state.errorMessage = action.error.message || ""
-    })
-  }
+      state.checkoutState = "ERROR";
+      state.errorMessage = action.error.message || "";
+    });
+  },
 });
 
 export const { addToCart, removeFromCart, udpateQuantity } = cartSlice.actions;
