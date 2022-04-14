650901293cbb75f9dc261421d206b4ab480dad19
Chanon Roy <chanonroy@gmail.com>
2022-04-14 22:47:01 +0100

    feat: add swc jest test

---
+++ swc-test/src/App.test.tsx
@@ -0,0 +1,7 @@
+import { App } from "./App";
+import { create } from "react-test-renderer";
+
+it("should work", () => {
+  const renderer = create(<App />);
+  expect(renderer.toJSON()).toBeDefined();
+});
