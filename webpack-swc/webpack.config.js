7d17a442049fb8e15d0464da1889d89961ea9f62
Chanon Roy <chanonroy@gmail.com>
2022-04-15 20:25:00 +0100

    add vendor chunking

--- webpack-swc/webpack.config.js
+++ webpack-swc/webpack.config.js
@@ -4,6 +4,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.ts',
+  // stats show logs for webpack dev server - 'minimal', 'verbose'
+  stats: 'minimal',
+  // contenthash will hash the filename everytime a change is made to trigger a browser cache miss
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
@@ -11,6 +14,7 @@ module.exports = {
   resolve: {
     extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
   },
+  // we are using swc loader here to transpile typescript into javascript
   module: {
     rules: [
       {
@@ -20,9 +24,11 @@ module.exports = {
       },
     ],
   },
+  // dev server allows us to have local development with hot reloading
   devServer: {
     port: 3005,
   },
+  // this helps us generate an html file with our bundle added to it
   plugins: [
     new HtmlWebpackPlugin({
       title: 'SWC example',
@@ -30,4 +36,10 @@ module.exports = {
       template: path.resolve(__dirname, 'src/index.html'),
     }),
   ],
+  // New in v5, we split vendor files automatically
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
 };
