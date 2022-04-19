a01a45e74ea2eb8547dc35627fb8aac46d0dfd77
Chanon Roy <chanonroy@gmail.com>
2022-04-19 12:41:34 +0100

    add index page example

--- rails_todo_app/db/seeds.rb
+++ rails_todo_app/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+todos = Todo.create([
+  {title: "Buy Groceries", description: "Go to Safeway, don't forget the coupons."},
+
+  {title: "Wash the Car", description: "Have a date this Saturday."},
+
+  {title: "Clean my room", description: "It's that time of year again."},
+
+  {title: "Do the Laundry", description: "Don't forget the bleach!"},
+
+  {title: "Work on Mini-Project", description: "What are Happy Tails anyway?"},
+
+  {title: "Walk the Dog", description: "Take Fido around the block."}
+
+   ])
