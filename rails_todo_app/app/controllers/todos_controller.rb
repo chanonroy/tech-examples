19b775443fc3352823a5393484ccc1030e8577d6
Chanon Roy <chanonroy@gmail.com>
2022-04-21 22:52:19 +0100

    rails: add create todo

--- rails_todo_app/app/controllers/todos_controller.rb
+++ rails_todo_app/app/controllers/todos_controller.rb
@@ -6,4 +6,18 @@ class TodosController < ApplicationController
     def show 
         @todo = Todo.find(params[:id])
     end
+
+    def new
+        @todo = Todo.new
+    end
+
+    def create
+        @todo = Todo.create todo_params
+        redirect_to todo_path(@todo)
+    end
+
+    private
+    def todo_params
+      params.require(:todo).permit(:title, :description)
+    end
 end
