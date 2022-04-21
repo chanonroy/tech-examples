c6ea030f734fbba694a201edb7bf25873e91634f
Chanon Roy <chanonroy@gmail.com>
2022-04-21 22:56:02 +0100

    rails: add edit todo

--- rails_todo_app/app/controllers/todos_controller.rb
+++ rails_todo_app/app/controllers/todos_controller.rb
@@ -11,6 +11,16 @@ class TodosController < ApplicationController
         @todo = Todo.new
     end
 
+    def edit
+        @todo = Todo.find(params[:id])
+    end
+
+    def update
+        @todo = Todo.find(params[:id])
+        @todo.update todo_params
+        redirect_to todo_path(@todo)
+    end
+
     def create
         @todo = Todo.create todo_params
         redirect_to todo_path(@todo)
