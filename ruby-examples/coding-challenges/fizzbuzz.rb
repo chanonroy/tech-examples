434ce5794c3d06650e913b5f424c5ef11857c1b1
Chanon Roy <chanonroy@gmail.com>
2023-01-21 20:48:50 +0000

    add fizzbuzz

---
+++ ruby-examples/coding-challenges/fizzbuzz.rb
@@ -0,0 +1,27 @@
+# divisible by 3 and 5 "FizzBuzz"
+# divisible by 3 "Fizz"
+# divisible by 5 "Buzz"
+
+def fizzbuzz(n)
+	if !(n.is_a? Integer) 
+		return
+	end
+
+	(1..n).map do |num|
+		fizz = (num % 3 == 0)
+   	buzz = (num % 5 == 0)
+
+		if fizz && buzz
+			"FizzBuzz"
+		elsif fizz
+			"Fizz"
+		elsif buzz
+			"Buzz"
+		else
+			num
+		end
+	end
+end
+
+result = fizzbuzz(15)
+print result
